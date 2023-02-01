662475c7c07ed1ce1bd30ba1a9a6b4873cef64e4
linaund1000 <89273434+linaund1000@users.noreply.github.com>
2023-02-01 15:31:12 +0300

    record for wallet without test

--- Wallet/wallet.go
+++ Wallet/wallet.go
@@ -2,32 +2,51 @@ package wallet
 
 import (
 	c "1000chain/crypto"
+	"fmt"
 	"time"
 )
+
 type Record struct {
-	IntoWallet map[time.Time]float64
-	FromWallet map[time.Time]float64
-	
+	IntoWallet map[int64]float64
+	FromWallet map[int64]float64
 }
 
+func (r *Record) Into(amound float64) {
+	time := time.Now().UnixNano()
+	r.IntoWallet[time] = amound
+}
+func (r *Record) From(amound float64) {
+	time := time.Now().UnixNano()
+	r.FromWallet[time] = amound
+}
 
-
-type Wallet struct {	
+type Wallet struct {
+	*Record
 	PubKey string
 	Stake  float64
 }
 
-
-func NewWallet()*Wallet{
+func NewWallet() *Wallet {
 	w := &Wallet{
+		Record: &Record{},
 		PubKey: "",
-		Stake : 0 ,
+		Stake:  0,
 	}
 	w.PubKey = c.GeneratePUBKEY()
-	return	w
+	return w
 }
 
-func (w *Wallet)AddStake(amount float64){
-
+func (w *Wallet) AddStake(amount float64) error {
 	w.Stake = amount + w.Stake
+	w.Into(amount)
+	return nil
+}
+func (w *Wallet) SubtractStake(amount float64) error {
+	if w.Stake < amount {
+		return	fmt.Errorf("not enough money in stake" ) 
+	}
+
+	w.Stake -= amount
+	w.From(amount)
+	return nil
 }
